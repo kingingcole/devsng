579439062e9fb22fd04da271eb7fd86fc5aee642
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-09-21 00:47:05 +0100

    ggg

--- src/templates/profile.js
+++ src/templates/profile.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 import PostCard from '../components/postCard'
 import { rhythm, scale } from "../utils/typography"
 import PostMetaData from "../components/postMetaData"
-import { COLORS } from "../utils/constants"
+import { COLORS, PAGE_MAX_WIDTH } from "../utils/constants"
 
 
 class ProfileTemplate extends React.Component {
@@ -21,42 +21,49 @@ class ProfileTemplate extends React.Component {
 
     const { name } = author.frontmatter
 
+    const PostsWrapper = styled.div`
+      max-width: ${PAGE_MAX_WIDTH};
+      margin: auto;
+      padding: 30px 10px
+    `
+
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title={name}
           description={`Profile of ${name} | DevsNg`}
         />
-        {posts.map(({node}) => {
-          const title = node.frontmatter.title || node.fields.slug
-          const description = node.frontmatter.description || node.excerpt
-          const {slug} = node.fields
-          let url = `post${slug}`
-          const {date} = node.frontmatter
-          const {author} = node.frontmatter
-          const readingTime = node.fields.readingTime.text;
-          const {featuredImage} = node.frontmatter;
+        <PostsWrapper className={`container`}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            const description = node.frontmatter.description || node.excerpt
+            const {slug} = node.fields
+            let url = `post${slug}`
+            const {date} = node.frontmatter
+            const {author} = node.frontmatter
+            const readingTime = node.fields.readingTime.text;
+            const {featuredImage} = node.frontmatter;
 
-          let image;
-          if (featuredImage){
-            image = node.frontmatter.featuredImage.childImageSharp.fluid
-          }
-          console.log(description)
+            let image;
+            if (featuredImage){
+              image = node.frontmatter.featuredImage.childImageSharp.fluid
+            }
 
-          return(
-            <PostCard
-              key={slug}
-              description={description}
-              title={title}
-              date={date}
-              url={url}
-              readingTime={readingTime}
-              image={image}
-              author={author}
-              hideAuthorDetails={true}
-            />
-          )
-        })}
+            return (
+              <PostCard
+                hideAuthorDetails={true}
+                key={slug}
+                description={description}
+                title={title}
+                date={date}
+                url={url}
+                readingTime={readingTime}
+                image={image}
+                author={author}
+              />
+            )
+          })}
+        </PostsWrapper>
       </Layout>
     )
   }
