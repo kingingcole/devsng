299d86c4de350fab9376e59e79caf82aab1184f4
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-09-25 01:18:47 +0100

    started profile page proper

---
+++ src/components/profileHeader.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import { COLORS } from "../utils/constants"
+
+const ProfileHeader = ({name}) => {
+  return (
+    <HeaderWrapper>
+      <ProfileName>{name}</ProfileName>
+    </HeaderWrapper>
+  )
+}
+
+const HeaderWrapper = styled.div`
+  padding: 20px 0;
+  background: ${COLORS.primaryColor};
+  font-family: 'Fira Sans', sans-serif;
+`
+
+const ProfileName = styled.p`
+  font-weight: bold
+`
+
+export default ProfileHeader
