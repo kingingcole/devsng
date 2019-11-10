0056fcd2423d092558158cd03c2bdad3edfd8c94
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-11-10 10:41:21 +0000

    Added google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,7 +92,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-151917102-1`,
       },
     },
     `gatsby-plugin-feed`,
